e087917004149bdff00eea07b6c3aa9da7f738b4
Andrey Sakhno <andrey.sakhno@gmail.com>
2020-10-15 13:48:47 +0300

    Get Human by Id

--- swapi/resolvers.py
+++ swapi/resolvers.py
@@ -3,3 +3,7 @@ from swapi.models import Human
 
 def resolver_humans():
     return Human.objects.all()
+
+
+def resolver_human(id):
+    return Human.objects.get(id=id)
--- swapi/schema.py
+++ swapi/schema.py
@@ -1,18 +1,23 @@
 import graphene
 from swapi.types import HumanType
-from swapi.resolvers import resolver_humans
+from swapi.resolvers import resolver_humans, resolver_human
 
 # 1
 class Query(graphene.ObjectType):
     # 2
     hello = graphene.String()
     humans = graphene.List(HumanType)
+    human = graphene.Field(HumanType, id=graphene.NonNull(graphene.Int))
+
     # 3
     def resolve_hello(self, info, **kwargs):
         return "world"
 
-    def resolve_humans(selfself, info):
+    def resolve_humans(self, info):
         return resolver_humans()
+
+    def resolve_human(self, info, id):
+        return resolver_human(id=id)
 
 
 # 4
