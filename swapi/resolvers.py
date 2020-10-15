8a137db4229cd58bcde0f3c98283cc2e1748bb96
Andrey Sakhno <andrey.sakhno@gmail.com>
2020-10-15 18:09:39 +0300

    Update human Mutation

--- swapi/resolvers.py
+++ swapi/resolvers.py
@@ -19,3 +19,15 @@ def resolver_create_human(id, name, gender, birth_year, mass, height, home_plane
         height=height,
         home_planet=home_planet,
     )
+
+
+def resolver_update_human(id, name, gender, birth_year, mass, height, home_planet):
+    human = Human.objects.get(id=id)
+    human.name = name
+    human.gender = gender
+    human.birth_year = birth_year
+    human.mass = mass
+    human.height = height
+    human.home_planet = home_planet
+    human.save()
+    return human
--- swapi/schema.py
+++ swapi/schema.py
@@ -1,15 +1,18 @@
 import graphene
 from swapi.types import HumanType, Gender
-from swapi.resolvers import resolver_humans, resolver_human, resolver_create_human
+from swapi.resolvers import (
+    resolver_humans,
+    resolver_human,
+    resolver_create_human,
+    resolver_update_human,
+)
 
-# 1
+
 class Query(graphene.ObjectType):
-    # 2
     hello = graphene.String()
     humans = graphene.List(HumanType)
     human = graphene.Field(HumanType, id=graphene.NonNull(graphene.Int))
 
-    # 3
     def resolve_hello(self, info, **kwargs):
         return "world"
 
@@ -36,6 +39,24 @@ class Mutation(graphene.ObjectType):
         self, info, id, name, gender, birth_year, mass, height, home_planet
     ):
         return resolver_create_human(
+            id, name, gender, birth_year, mass, height, home_planet
+        )
+
+    update_human = graphene.Field(
+        HumanType,
+        id=graphene.NonNull(graphene.Int),
+        name=graphene.NonNull(graphene.String),
+        gender=graphene.NonNull(Gender),
+        birth_year=graphene.NonNull(graphene.String),
+        mass=graphene.Int(),
+        height=graphene.Int(),
+        home_planet=graphene.String(),
+    )
+
+    def resolve_update_human(
+        self, info, id, name, gender, birth_year, mass, height, home_planet
+    ):
+        return resolver_update_human(
             id, name, gender, birth_year, mass, height, home_planet
         )
 
